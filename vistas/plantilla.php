18d165975a734850de3dd3d11da6d89fda0e7416
Jose Luis Torres <java.mx@gmail.com>
2018-05-18 10:56:38 -0400

    commit por reinicio

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -39,9 +39,21 @@
   <!-- Site wrapper -->
   <div class="wrapper">
     <?php
+    
     include "modulos/cabezote.php";
     include "modulos/menu.php";
-    include "modulos/contenido.php";
+    if(isset($_GET["ruta"])){
+      if ($_GET["ruta"] == "inicio" ||
+          $_GET["ruta"] == "usuarios" ||
+          $_GET["ruta"] == "categorias" ||
+          $_GET["ruta"] == "productos" ||
+          $_GET["ruta"] == "clientes" ||
+          $_GET["ruta"] == "ventas" ||
+          $_GET["ruta"] == "crear-venta" ||
+          $_GET["ruta"] == "reportes" ||) {
+        include "modulos/".$_GET["ruta"].".php";
+      }
+    }
     include "modulos/footer.php";
   ?>
 
